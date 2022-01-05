470aa6027efddaab27e9eb6d5736c68935390553
back-jond1992 <b.jond7@outlook.com>
2022-01-05 13:10:04 +0000

    fixes codes of pig latin for edge cases

--- 5kyu.js
+++ 5kyu.js
@@ -5,9 +5,13 @@ function pigIt(str) {
 
   return individualWords
     .map((word) => {
-      const firstLetter = word.slice(0, 1);
-      const restOfWord = word.slice(1);
-      return restOfWord + firstLetter + "ay";
+      if (word.match(/^[a-zA-Z]+$/g)) {
+        const firstLetter = word.slice(0, 1);
+        const restOfWord = word.slice(1);
+        return restOfWord + firstLetter + "ay";
+      } else {
+        return word;
+      }
     })
     .join(" ");
 }
